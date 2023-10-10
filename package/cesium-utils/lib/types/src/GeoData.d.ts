611f40d626a3103cc9049477498d2480e8cd9ae3
Supershuu <2426230872@qq.com>
2023-10-10 15:26:42 +0800

    :bento: 修改.gitignore

--- package/cesium-utils/lib/types/src/GeoData.d.ts
+++
@@ -1,3 +0,0 @@
-import { Viewer } from 'cesium';
-declare const geoMark: (url: string, viewer: Viewer) => void;
-export { geoMark };
--- package/cesium-utils/lib/types/src/dam.d.ts
+++
@@ -1,3 +0,0 @@
-import { Viewer } from 'cesium';
-declare const createDam: (lon: number, lat: number, height: number, url: string, viewer: Viewer) => void;
-export { createDam };
